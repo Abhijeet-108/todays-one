6867a62c48f0062dd924661aa4e0c63a5872cdb6
Abhijeet <118212415+Abhijeet-108@users.noreply.github.com>
2023-02-01 22:30:48 +0530

    Update lapi.cpp

--- lapi.cpp
+++ lapi.cpp
@@ -2,18 +2,15 @@
 using namespace std;
 # define tax 0.60;
 int main(){
+     int sr{0},lr{0},tt,te;
     cout<< "Frank's Carpet Cleaning Servies"<<endl;
     cout<<"No. of small room need: ";
-    int sr{0};
     cin >> sr;
     cout<<"\nNo. of Large room need: ";
-    int lr{0};
     cin >> lr;
-    int tt;
     tt = ((lr*35) + (sr*25)) * tax;
     cout << "Cost: $"<<(lr*35) + (sr*25);
     cout << endl <<"Total Tax: $"<< tt << endl;
-    int te;
     te = (lr*35) + (sr*25) + tt;
     cout <<"-----------------------------------------" ;
     cout<< "\nTotal Estimate:- $"<< te;
